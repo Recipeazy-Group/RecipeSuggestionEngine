4186ced01d42ce852458bd4d244c49076cff86bc
Morgan_Server <vandenberg.morgan@gmail.com>
2019-10-08 13:31:23 -0500

    Adding vector loading files

--- src/Driver/SuggestionEngine.java
+++ src/Driver/SuggestionEngine.java
@@ -1,8 +1,15 @@
 package Driver;
 
+import Util.WordVector;
+
 public class SuggestionEngine {
 
 
-
+    public static void main(String[] args) {
+        WordVector vectors = new WordVector("RecipeSuggestionEngine/lib/models/GoogleNews-vectors-negative300.bin.gz");
+        System.out.println(vectors.getClosestMatches("bourbon", 10));
+        System.out.println();
+        System.out.println(vectors.getClosestMatches("apple", 10));
+    }
 
 }
