23d440187a99f048b7c1688b865d536d2fe757fb
MorganV <vandenberg.morgan@gmail.com>
2019-10-08 15:04:37 -0500

    Adding Vector for math

---
+++ src/Util/Math/Vector.java
@@ -0,0 +1,45 @@
+package Util.Math;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class Vector<T> {
+    private T[] data;
+
+    private final Class type;
+
+    private final int size;
+
+    public Vector(List<T> data) {
+        size = data.size();
+        type = data.get(0).getClass();
+        for (int i = 0; i < data.size(); i++) {
+            this.data[i] = data.get(i);
+        }
+    }
+
+    public Vector(T[] data) {
+        this(Arrays.asList(data));
+    }
+
+    public T at(int index) {
+        return data[index];
+    }
+
+    public static double dotProduct(Vector a, Vector b) {
+        if (a.size != b.size)
+            throw new RuntimeException("Vector: dot product attempted on vectors of different sizes.");
+        if (a.type != b.type || (b.type != Double.class && b.type != Integer.class && b.type != Long.class))
+            throw new RuntimeException("Vector: current element type " + a.type + " is not supported by dotProduct(..)");
+        double sum = 0;
+        for (int i = 0; i < a.size; i++) {
+            sum += (double)a.data[i] * (double)b.data[i];
+        }
+        return sum;
+    }
+
+    public double dotProduct(Vector b) {
+        return dotProduct(this, b);
+    }
+
+}
